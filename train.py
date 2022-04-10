fe516b84f46f0ee0ac109a93c9518d514514e2ee
Daniil Zhilin <d.zhilin2013@yandex.ru>
2022-04-10 23:35:16 +0300

    [solv-1] Add readme with important info

--- train.py
+++ train.py
@@ -1,13 +1,30 @@
-from catboost import CatBoostRegressor, CatBoost, Pool
+from catboost import CatBoostRegressor, Pool
 import pandas as pd
 import yaml
 
 import pickle
 import logging
-import src
+# import src
 
 config = yaml.safe_load(open('config/params.yaml'))
 path_load = config['load']['path']['load']
 path_write = config['load']['path']['write']
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+print(path_load)
+
+
+def load_data() -> (pd.DataFrame, pd.DataFrame):
+    train_data = pd.read_csv(f'{path_load}/ds_dask_data.csv', sep=';')
+    labels = pd.read_csv(f'{path_load}/target.csv', sep=';')
+    return train_data, labels
+
+
+def transform_data():
+    data, target = load_data()
+    data.dt = pd.to_datetime(data.dt, format="%Y%m%d")
+    target.dt = pd.to_datetime(target.dt, format="%Y%m%d")
+    merged = data.merge(right=target, suffixes=("_caller", '_other'), on=['geo_h3_10', 'dt'], how='left')
+
+
 
